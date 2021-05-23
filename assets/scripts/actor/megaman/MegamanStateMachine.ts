a683db784bb12328137d7dcdb1c1493ea2445907
Z1wolfmaster1 <suiciniv2016@outlook.com>
2021-05-22 21:16:25 -0300

    chore: animations transitions are now made in the onAnimationEnd function

--- assets/scripts/actor/megaman/MegamanStateMachine.ts
+++ assets/scripts/actor/megaman/MegamanStateMachine.ts
@@ -7,10 +7,28 @@ const { ccclass } = cc._decorator;
 @ccclass
 export default class MegamanStateMachine extends StateMachine<MEGAMAN_STATES, Megaman> {
   public onAnimationStart(state: MEGAMAN_STATES): void {
-    throw new Error('Method not implemented.');
+    switch (state) {
+      default:
+        console.log(state);
+        break;
+    }
   }
 
   public onAnimationEnd(state: MEGAMAN_STATES): void {
-    throw new Error('Method not implemented.');
+    switch (state) {
+      case MEGAMAN_STATES.JUMP:
+        this.node.getComponent(cc.Animation).play(MEGAMAN_STATES.FALLING);
+        break;
+      case MEGAMAN_STATES.FALLING:
+        this.node.getComponent(cc.Animation).play(MEGAMAN_STATES.LANDING);
+        break;
+      case MEGAMAN_STATES.LANDING:
+        this.node.getComponent(cc.Animation).play(MEGAMAN_STATES.IDLE);
+        break;
+
+      default:
+        console.log(state);
+        break;
+    }
   }
 }
