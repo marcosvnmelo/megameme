1100a4bb1be3ba9a4d84a83ed1274727c963c0d1
Z1wolfmaster1 <suiciniv2016@outlook.com>
2021-05-26 10:15:48 -0300

    chore: collisions enabled

--- assets/scripts/Main.ts
+++ assets/scripts/Main.ts
@@ -4,9 +4,13 @@ const { ccclass } = cc._decorator;
 export default class Main extends cc.Component {
   private physicsManager: cc.PhysicsManager;
 
+  private collisionManager: cc.CollisionManager;
+
   public onLoad(): void {
     this.physicsManager = cc.director.getPhysicsManager();
     this.physicsManager.enabled = true;
     this.physicsManager.gravity = cc.v2(0, -2000);
+    this.collisionManager = cc.director.getCollisionManager();
+    this.collisionManager.enabled = true;
   }
 }
