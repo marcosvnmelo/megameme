261676ce425c9b936d95b993e19aa87a65213167
Z1wolfmaster1 <suiciniv2016@outlook.com>
2021-06-16 18:00:50 -0300

    feat: Gun Volt states enum created

---
+++ assets/scripts/actor/enemies/gun_volt/GUN_VOLT_STATES.ts
@@ -0,0 +1,9 @@
+// eslint-disable-next-line no-shadow
+enum GUN_VOLT_STATES {
+  IDLE = 'idle',
+  LOADING = 'loading',
+  SHOOTING = 'shooting',
+  CLOSING = 'closing',
+}
+
+export default GUN_VOLT_STATES;
